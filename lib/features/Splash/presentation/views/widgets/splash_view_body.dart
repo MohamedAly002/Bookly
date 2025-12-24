c53f01649548bf9af9df076af91e494f4e562843
MohamedAly002 <mo7amed3ly77@gmail.com>
2025-12-24 02:25:19 +0200

    finish splash view & create home view

--- lib/features/Splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/Splash/presentation/views/widgets/splash_view_body.dart
@@ -1,7 +1,10 @@
+import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/assets.dart';
+import 'package:bookly/features/Home/presentation/view/home_view.dart';
 import 'package:bookly/features/Splash/presentation/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -10,23 +13,18 @@ class SplashViewBody extends StatefulWidget {
   State<SplashViewBody> createState() => _SplashViewBodyState();
 }
 
-
-
-class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProviderStateMixin {
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
   late AnimationController animationController;
-late Animation<Offset> slidingAnimation;
+  late Animation<Offset> slidingAnimation;
   @override
   void initState() {
     initSlidingText();
-   
+    navigationToHomeView();
+
     super.initState();
   }
 
-  void initSlidingText() {
-      animationController = AnimationController(vsync: this, duration: const Duration(seconds: 1  ));
-    slidingAnimation = Tween<Offset>(begin: const Offset(0, 3), end: Offset.zero).animate(animationController);
-    animationController.forward();
-  }
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -41,5 +39,20 @@ late Animation<Offset> slidingAnimation;
       ],
     );
   }
-}
 
+  Future<Null> navigationToHomeView() {
+    return Future.delayed(const Duration(seconds: 3), () {
+      Get.to(() => const HomeView(),
+          transition: Transition.fade, duration: kTransitionDuration);
+    });
+  }
+
+  void initSlidingText() {
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 3), end: Offset.zero)
+            .animate(animationController);
+    animationController.forward();
+  }
+}
