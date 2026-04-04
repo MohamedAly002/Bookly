dd9ae3e67b46a59139ddff75979394c9bb9f9314
mohamedaly0206 <mohamedaly0206@gmail.com>
2026-04-04 18:24:00 +0200

    add router paths and delete unused base response class

--- lib/config/app_routers/app_routers.dart
+++ lib/config/app_routers/app_routers.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/config/app_routers/router_paths.dart';
 import 'package:bookly/features/Home/domain/models/items_model.dart';
 import 'package:bookly/features/Home/presentation/views/book_details_view.dart';
 import 'package:bookly/features/Home/presentation/views/home_view.dart';
@@ -6,26 +7,24 @@ import 'package:bookly/features/Splash/presentation/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouters {
-  static const homeView = '/homeView';
-  static const bookDetailsView = '/bookDetailsView';
-  static const searchView = '/searchView';
+ 
   static final router = GoRouter(
     routes: [
       GoRoute(
-        path: '/',
+        path: RouterPaths.splashView,
         builder: (context, state) => const SplashView(),
       ),
       GoRoute(
-        path: homeView,
+        path: RouterPaths.homeView,
         builder: (context, state) => const HomeView(),
       ),
       GoRoute(
-          path: bookDetailsView,
+          path: RouterPaths.bookDetailsView,
           builder: (context, state) => BookDetailsView(
                 bookItem: state.extra as ItemsModel,
               )),
       GoRoute(
-          path: searchView, builder: (context, state) => const SearchView()),
+          path: RouterPaths.searchView, builder: (context, state) => const SearchView()),
     ],
   );
 }
---
+++ lib/config/app_routers/router_paths.dart
@@ -0,0 +1,6 @@
+class RouterPaths {
+  static const homeView = '/homeView';
+  static const bookDetailsView = '/bookDetailsView';
+  static const searchView = '/searchView';
+  static const splashView = '/';
+}
